e43af5f7e75741adb286f7fe398ff334e6e52f88
yeondg <zzmn1234@naver.com>
2023-12-28 23:14:47 +0900

    feat: 트위치 채팅 구현중

--- app/(platform)/twitch/_components/tw-chat-item.tsx
+++ app/(platform)/twitch/_components/tw-chat-item.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const TwChatItem = () => {
-  return <div>TwChatItem</div>;
+  return (
+    <div className='flex flex-col justify-start  w-[320px] mx-auto  hover:bg-slate-700/40 rounded-md '>
+      <div className='flex px-2.5'>
+        <p className='text-xs leading-5 w-full font-medium'>
+          <span className='rounded-sm font-bold hover:bg-slate-200 cursor-pointer'>
+            도리도리 <span className='font-normal'>(dodory123)</span>
+          </span>
+          : 안녀아세안오오오오오오오오오오오오하리아리아리
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default TwChatItem;
--- app/(platform)/twitch/_components/tw-chat-items.tsx
+++ app/(platform)/twitch/_components/tw-chat-items.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import TwChatItem from './tw-chat-item';
 
 export const TwChatItems = () => {
   return (
-    <div className='flex flex-col w-full h-full'>
+    <div className='flex flex-col-reverse justify-start w-full h-full py-2'>
+      <TwChatItem />
+      <TwChatItem />
+      <TwChatItem />
+      <TwChatItem />
       <TwChatItem />
     </div>
   );
