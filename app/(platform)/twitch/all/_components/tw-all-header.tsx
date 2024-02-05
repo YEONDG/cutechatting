61ca377f715a558cb2cc2bd53398dd1113cffeff
YDG <zzmn1234@naver.com>
2024-02-05 21:35:34 +0900

    refactor: 트위치게시물 라우트 리팩토링

--- app/(platform)/twitch/all/_components/tw-all-header.tsx
+++ app/(platform)/twitch/all/_components/tw-all-header.tsx
@@ -1,5 +1,13 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import React from 'react';
 
 export const TwAllHeader = () => {
-  return <div>TwAllHeader</div>;
+  return (
+    <div>
+      <Button>
+        <Link href={'/twitch'}>인증게시판 돌아가기</Link>
+      </Button>
+    </div>
+  );
 };
--- app/api/twitch/route.ts
+++ app/api/twitch/route.ts
@@ -12,57 +12,43 @@ export async function GET(req: Request) {
     if (page === '0')
       return new NextResponse('post가 없습니다!', { status: 400 });
 
+    const ITEMS_PER_PAGE = 6;
     const pageNumber = Number(page);
     const whereCondition = approved === 'true' ? { approved: true } : {};
 
-    if (popular === 'false') {
-      const twitchPosts = await db.twitchPost.findMany({
-        where: whereCondition,
-        skip: (pageNumber - 1) * 6,
-        take: 6,
-        orderBy: {
-          id: 'desc',
-        },
-        include: {
-          likes: true,
-          tags: true,
-          author: {
-            select: {
-              username: true,
+    const orderBy =
+      popular === 'true'
+        ? [
+            {
+              likes: {
+                _count: 'desc' as const,
+              },
             },
-          },
-        },
-      });
+            {
+              id: 'desc' as const,
+            },
+          ]
+        : {
+            id: 'desc' as const,
+          };
 
-      return NextResponse.json(twitchPosts);
-    } else if (popular === 'true') {
-      const twitchPosts = await db.twitchPost.findMany({
-        where: whereCondition,
-        skip: (pageNumber - 1) * 6,
-        take: 6,
-        orderBy: [
-          {
-            likes: {
-              _count: 'desc',
-            },
-          },
-          {
-            id: 'desc',
-          },
-        ],
-        include: {
-          likes: true,
-          tags: true,
-          author: {
-            select: {
-              username: true,
-            },
+    const twitchPosts = await db.twitchPost.findMany({
+      where: whereCondition,
+      skip: (pageNumber - 1) * 6,
+      take: 6,
+      orderBy: orderBy,
+      include: {
+        likes: true,
+        tags: true,
+        author: {
+          select: {
+            username: true,
           },
         },
-      });
+      },
+    });
 
-      return NextResponse.json(twitchPosts);
-    }
+    return NextResponse.json(twitchPosts);
   } catch (error) {
     console.log('[Twitch_Posts_GET]', error);
     return new NextResponse('Internal Error', { status: 500 });
