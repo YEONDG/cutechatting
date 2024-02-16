2d9fd0ebb2aa10a0b795a87c99fe3e6839562324
YDG <zzmn1234@naver.com>
2024-02-17 03:26:31 +0900

    refactor: 좋아요 hooks 분리

--- app/(platform)/twitch/_components/tw-main-card.tsx
+++ app/(platform)/twitch/_components/tw-main-card.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import React, { memo, useEffect, useState } from 'react';
-import { ClipboardCopy, Edit, Trash2 } from 'lucide-react';
+import React from 'react';
+import { Check, ClipboardCopy } from 'lucide-react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { toast } from 'sonner';
 import type { Like, Tag, UserRole } from '@prisma/client';
 
 import { Button } from '@/components/ui/button';
@@ -13,9 +12,9 @@ import { CardHeader } from '@/components/card/card-header';
 import { LikeButton } from '@/components/like-button';
 import { CardContent } from '@/components/card/card-content';
 import { TagDisplay } from '@/components/tag-display';
-import { updateLiked } from '@/apis/twitch/post';
 import { CardEditBtn } from '@/components/card/card-edit-btn';
 import { CardDeleteBtn } from '@/components/card/card-delete-btn';
+import useLike from '@/hooks/useLike';
 
 interface TwMainCardProps {
   id: number;
@@ -31,90 +30,77 @@ interface TwMainCardProps {
   role?: UserRole;
 }
 
-// eslint-disable-next-line react/display-name
-export const TwMainCard = memo(
-  ({
-    id,
-    title,
-    content,
-    authorId,
-    userId,
-    createdAt,
-    likes = [],
-    tags = [],
-    username,
-    approved,
-    role = 'USER',
-  }: TwMainCardProps) => {
-    const { copy, onCopySuccess } = useCopyToClipboard();
-    const [isLiked, setIsLiked] = useState(
-      likes.some((item) => item.userId === userId)
-    );
-    const [likeCount, setLikeCount] = useState(likes.length);
+export const TwMainCard = ({
+  id,
+  title,
+  content,
+  authorId,
+  userId,
+  createdAt,
+  likes = [],
+  tags = [],
+  username,
+  approved,
+  role = 'USER',
+}: TwMainCardProps) => {
+  const { copy, onCopySuccess } = useCopyToClipboard();
 
-    const createdDate = new Date(createdAt).toLocaleDateString();
-    const IsAuthor = authorId === userId;
-    const IsAdmin = role === 'ADMIN';
+  const { isLiked, likeCount, toggleLike } = useLike(id, likes, userId);
 
-    useEffect(() => {
-      setIsLiked(likes.some((item) => item.userId === userId));
-    }, [likes, userId]);
+  const createdDate = new Date(createdAt).toLocaleDateString();
+  const IsAuthor = authorId === userId;
+  const IsAdmin = role === 'ADMIN';
 
-    const handleLikeClick = async (postId: number) => {
-      try {
-        const data = await updateLiked(postId);
-        setIsLiked(data['isLiked']);
-        setLikeCount(data['likeCount'].length);
-        toast.success('좋아요 성공');
-      } catch (error) {
-        console.error('Error while processing like:', (error as Error).message);
-        toast.error('로그인이 필요합니다.');
-      }
-    };
-
-    return (
-      <section className='relative flex flex-col justify-between border max-w-[350px] mx-auto border-black text-xs  dark:border-white'>
-        <CardHeader
-          id={id}
-          title={title}
-          username={username ?? ''}
-          createdDate={createdDate}
-          approved={approved}
+  return (
+    <section className='relative flex flex-col justify-between border max-w-[350px] mx-auto border-black text-xs  dark:border-white'>
+      <CardHeader
+        id={id}
+        title={title}
+        username={username ?? ''}
+        createdDate={createdDate}
+        approved={approved}
+      />
+      <div className='absolute top-2 right-0'>
+        <LikeButton
+          isLiked={isLiked}
+          likeCount={likeCount}
+          onLikeClick={toggleLike}
         />
-        <div className='absolute top-2 right-0'>
-          <LikeButton
-            isLiked={isLiked}
-            likeCount={likeCount}
-            onLikeClick={() => handleLikeClick(id)}
-          />
-        </div>
-        <Separator className='mb-2' />
-        <CardContent content={content} />
-        <TagDisplay tags={tags} />
-        <Separator />
-        <div className='flex justify-between items-center mx-4 my-2 gap-4'>
+      </div>
+      <Separator className='mb-2' />
+      <CardContent content={content} />
+      <TagDisplay tags={tags} />
+      <Separator />
+      <div className='flex justify-between items-center mx-2 my-2 gap-2'>
+        <div className='flex'>
           <CopyToClipboard text={content ?? ''} onCopy={onCopySuccess}>
             <Button variant='default' className='w-24 h-8'>
               <ClipboardCopy />
               Copy
             </Button>
           </CopyToClipboard>
-          {copy && <div className='text-lg'>복사 완료</div>}
-          {(IsAuthor || IsAdmin) && (
-            <div className='flex gap-2'>
-              <CardEditBtn
-                postId={id}
-                title={title}
-                content={content}
-                tags={tags}
-                userId={userId}
-              />
-
-              <CardDeleteBtn postId={id} />
-            </div>
+          {copy && (
+            <p className='flex text-xl'>
+              <Check className='text-green-500 w-8 h-8' />
+              복사완료
+            </p>
           )}
         </div>
-      </section>
-    );
-  }
-);
+
+        {(IsAuthor || IsAdmin) && (
+          <div className='flex gap-2'>
+            <CardEditBtn
+              postId={id}
+              title={title}
+              content={content}
+              tags={tags}
+              userId={userId}
+            />
+
+            <CardDeleteBtn postId={id} />
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
