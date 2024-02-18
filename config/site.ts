a2f5d752b603a2ae8dce9a3e8f9a1b746c1d1d8b
YDG <zzmn1234@naver.com>
2024-02-18 18:54:06 +0900

    fix: 사이트 이름 변경

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: 'cutechat',
-  url: 'https://ganda.vercel.app/',
+  url: 'https://cutechatting.com',
   ogImage: '/cutechat.png',
   description: '방송 플랫폼 이모티콘 채팅모음 사이트입니다.',
 };
