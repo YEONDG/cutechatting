a97576de433d653249f13f4ecd885acb0d061dc0
YDG <zzmn1234@naver.com>
2024-02-19 00:32:16 +0900

    fix: 재배포

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { getToken } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic';
-
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
 
