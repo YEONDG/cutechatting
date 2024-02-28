ce9f8eeb12110a9f9ec66f40afcddaa69a92996f
YDG <zzmn1234@naver.com>
2024-02-28 19:20:25 +0900

    fix: dashboard 레이아웃 수정

--- app/(myPage)/dashboard/layout.tsx
+++ app/(myPage)/dashboard/layout.tsx
@@ -18,14 +18,14 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <section className="container w-full px-2">
+    <section className="w-full px-2">
       <h2 className="text-3xl">대시보드</h2>
       <Separator className="my-4" />
-      <div className="flex flex-col lg:flex-row lg:space-x-12 lg:space-y-0">
+      <div className="flex w-screen max-w-7xl flex-col lg:flex-row lg:space-x-12 lg:space-y-0">
         <aside className="-mx-4 lg:w-1/5">
           <SidebarNav items={sidebarNavItems} />
         </aside>
-        <div className="flex-1 lg:w-4/5">{children}</div>
+        <div className="flex-1 lg:w-4/5 ">{children}</div>
       </div>
     </section>
   )
